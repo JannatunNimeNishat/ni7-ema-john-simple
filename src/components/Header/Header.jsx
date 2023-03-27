a4c4c4a5e9aee5c48a87ed0bc4efda433414f08d
JannatunNimeNishat <nishatnime100@gmail.com>
2023-03-27 11:44:20 +0600

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//css
+import './Header.css'
+//img
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div className='nav-item'>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
