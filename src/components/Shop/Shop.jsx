837322f45f25c371b48f1eeac00753c7febdd265
JannatunNimeNishat <nishatnime100@gmail.com>
2023-03-27 12:59:36 +0600

    added products

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+//css
+import './Shop.css'
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('../../../../public/products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                {
+                    products.map(product => <Product
+                        product={product}
+                        key={product.id}></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h4>Order summary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
