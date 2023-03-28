2648a1485eb6d618b41fef6646c734447cb6e9d4
JannatunNimeNishat <nishatnime100@gmail.com>
2023-03-28 16:27:04 +0600

    retrive product from stored cart and set it to cart setate

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,23 +14,54 @@ const Shop = () => {
     }, [])
 
     //2nd
-    useEffect(()=>{
+    /* useEffect(()=>{
+        // console.log(products);
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    },[])
+        //step 1: get id
+        for(const id in storedCart){
+            //step 2 get the product using id
+            const addedProduct = products.find(product => product.id == id);
+            //step 3: get quantity of the product
+             const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+           console.log(addedProduct);
+        }
+    },[products]) */
 
 
+    //2nd
+    useEffect(() => {
+        //step 1: get data from local storage
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // console.log(addedProduct);
+        for (const id in storedCart) {
+            //step 2: get product from products state by using id
+            const addedProduct = products.find(product => product.id == id);
+            if (addedProduct) {
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                //step 4: add the addedProduct to the savedCart array
+                savedCart.push(addedProduct)
+            }
+            console.log(id, ":", storedCart[id], ":", addedProduct);
+        }
+        console.log('savedCart',savedCart);
+        //step 5 set the cart
+        setCart(savedCart)
+    }, [products])
 
 
 
 
     // event handler for product
-    const handelAddToCart = (product) =>{
-       
+    const handelAddToCart = (product) => {
+
         const newCart = [...cart, product]
         setCart(newCart);
         addToDb(product.id)
- 
+
     }
     return (
         <div className='shop-container'>
@@ -39,12 +70,12 @@ const Shop = () => {
                     products.map(product => <Product
                         product={product}
                         key={product.id}
-                        handelAddToCart = {handelAddToCart}
-                        ></Product>)
+                        handelAddToCart={handelAddToCart}
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
-              <Cart cart={cart}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
